07a6c4814668a43feab0e4c3ce2b6fd8f5a1c802
shathakh <shathaqudaih17@gmail.com>
2022-03-02 22:23:21 +0200

    create add function to add task list in the ul relates #12

--- script.js
+++ script.js
@@ -5,5 +5,20 @@ const todoList= document.querySelector('.todo-list');
 //add action listener
 todoButton.addEventListener('click', addNewItem);
 
+//function to add task list into the ul
+function addNewItem(e){
 
+    e.preventDefault();
+   
+    let newDiv= document.createElement('div')
+      let newItem = `
+        <li>${todoInput.value}</li>
+        <i class="fa-solid fa-pen"></i>
+        <i class="fa-solid fa-trash"></i>`;
+
+      newDiv.innerHTML= newItem;
+      todoList.appendChild(newDiv)
+      newDiv.classList.add('todo-item') //add style for div
+    todoInput.value=""; //!once task added leave the input blank
+}
 
